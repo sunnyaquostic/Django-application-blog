8370dcd0cb374b9c658cad4e2a8001d6ff325bf7
Sunday Ige <sunnyaquostic@gmail.com>
2025-04-24 19:07:28 +0100

    add basic crud operation to the blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("post/<int:pk>", views.post_details, name="post-detail"),
+    path("post/new/", views.PostcreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete")
 ]
